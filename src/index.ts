806d4e626b0f640abf2ac7cb70886c41c0a8b0d6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-07-22 16:46:31 -1000

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,102 @@
+import { Configuration as WebpackConfiguration } from "webpack";
+
+/**
+ * Options for the addon.
+ */
+interface AddonOptions {
+	/**
+	 * Determines if TypeScript support is included.
+	 *
+	 * @default true
+	 */
+	withTypescript?: boolean;
+}
+
+/**
+ * This adds a file necessary to make the preview environment compatible with Gatsby.
+ *
+ * @param entries Existing entries
+ * @param options Options specific to this addon.
+ *
+ * @returns `entries` with the Gatsby-specific configuration.
+ */
+export const config = (
+	entries: string[] = [],
+	_options: AddonOptions,
+): string[] => {
+	return [...entries, require.resolve("./preview.mjs")];
+};
+
+/**
+ * This modifies the Webpack configuration to simulate Gatsby's internal Webpack configuration. It is derived from Gatsby's Visual Testing with Storybook guide and a collective effort of Gatsby users.
+ *
+ * {@link https://www.gatsbyjs.com/docs/how-to/testing/visual-testing-with-storybook/}
+ * {@link https://github.com/gatsbyjs/gatsby/issues/26099}
+ *
+ * @param config The incoming Webpack configuration object.
+ * @param options Options specific to this addon.
+ *
+ * @returns `config` modified to simulate Gatsby's internal Webpack configuration.
+ */
+export const webpack = (
+	config: WebpackConfiguration,
+	options: AddonOptions,
+): WebpackConfiguration => {
+	const babelPlugins = [
+		// use @babel/plugin-proposal-class-properties for class arrow functions
+		require.resolve("@babel/plugin-proposal-class-properties"),
+		// use @babel/plugin-proposal-optional-chaining since Webpack 4 doesn't understand optional chaining
+		require.resolve("@babel/plugin-proposal-optional-chaining"),
+		// use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+		[
+			require.resolve("babel-plugin-remove-graphql-queries"),
+			{
+				stage: config.mode === `development` ? "develop-html" : "build-html",
+				staticQueryDir: "page-data/sq/d",
+			},
+		],
+	];
+
+	if (typeof config.module?.rules?.[0] === "object") {
+		// Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+		config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+
+		if (Array.isArray(config.module.rules[0].use)) {
+			if (typeof config.module.rules[0].use[0] === "object") {
+				// use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+				config.module.rules[0].use[0].loader = require.resolve("babel-loader");
+
+				// use @babel/preset-react for JSX and env (instead of staged presets)
+				if (typeof config.module.rules[0].use[0].options === "object") {
+					config.module.rules[0].use[0].options.presets = [
+						require.resolve("@babel/preset-react"),
+						require.resolve("@babel/preset-env"),
+					];
+
+					config.module.rules[0].use[0].options.plugins = babelPlugins;
+				}
+			}
+		}
+	}
+
+	// Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+	if (config.resolve) {
+		config.resolve.mainFields = ["browser", "module", "main"];
+	}
+
+	// Add TypeScript support if enabled
+	if (options.withTypescript === true) {
+		config.module?.rules?.push({
+			test: /\.(ts|tsx)$/,
+			loader: require.resolve("babel-loader"),
+			options: {
+				presets: [["react-app", { flow: false, typescript: true }]],
+				plugins: babelPlugins,
+			},
+		});
+
+		config.resolve?.extensions?.push(".ts", ".tsx");
+	}
+
+	return config;
+};
---
+++ src/preview.ts
@@ -0,0 +1,41 @@
+// Needed to declare types in the `global` namespace. This identifies the file
+// as a module.
+export {};
+
+declare global {
+	// eslint-disable-next-line no-var
+	var ___loader: {
+		enqueue: () => void;
+		hovering: () => void;
+	};
+	// eslint-disable-next-line no-var
+	var __PATH_PREFIX__: string;
+	// eslint-disable-next-line no-var
+	var __BASE_PATH__: string;
+	// eslint-disable-next-line no-var
+	var ___navigate: (pathname: string) => void;
+}
+
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from
+// creating console errors you override it here
+globalThis.___loader = {
+	enqueue: () => {
+		// noop
+	},
+	hovering: () => {
+		// noop
+	},
+};
+
+// Gatsby internal mocking to prevent unnecessary errors in Storybook testing
+// environment
+window.__PATH_PREFIX__ = "";
+window.__BASE_PATH__ = "";
+
+// This is to utilized to override the window.___navigate method Gatsby defines
+// and uses to report what path a Link would be taking us to if it wasn't
+// inside a storybook
+window.___navigate = (pathname) => {
+	console.info("Navigate to: ", pathname);
+};
